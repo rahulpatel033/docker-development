5de3509640c025bfee249a076266813032747974
rahulpatel033 <rahulpatel033@gmail.com>
2018-10-15 18:58:06 +0530

    initial commit

---
+++ html/php-app/code/test.php
@@ -0,0 +1,16 @@
+<?php
+// hostname should be container's Id
+$link = mysqli_connect("5a492f3d6998", "root", "password", "db");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
